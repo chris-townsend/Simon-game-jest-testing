8f8f66c24d8b199b78e9ba1385b366489ea74884
chris-townsend <chris-townsend@live.co.uk>
2022-10-06 11:43:02 +0000

    Add game object tests

---
+++ scripts/game.js
@@ -0,0 +1,12 @@
+/*
+*@jest-environment jsdom
+*/
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+module.exports = { game };
